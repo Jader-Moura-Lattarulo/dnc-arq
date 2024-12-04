c50af20de43d61ddaa30b3f855463b5d8b6299cb
Jader Moura <jader.dev.moura@gmail.com>
2024-12-04 12:19:32 -0300

    componente hero, pronto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Banner from "../components/Banner/Banner"
 import Button from "../components/Button/Button"
 import Header from "../components/Header/Header"
+import Hero from "../components/Hero/Hero"
 import Footer from "../components/Footer/Footer"
 
 function Home() {
@@ -10,7 +11,9 @@ function Home() {
             <h1>Home</h1>
             <p>Subtitle</p>
             <Button buttonStyle="primary" arrow>Olá!</Button>
-            <Banner title="Home" image="about.jpg"/>
+            <div className="container">
+                <Hero/>
+            </div>
             <Footer />
         </>
     )
