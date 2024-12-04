d409c202635c715bbbf607810e4a76be03607248
Jader Moura <jader.dev.moura@gmail.com>
2024-12-04 11:08:23 -0300

    Componente botão finalizado

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,9 +1,10 @@
 import './Button.css'
+import WhiteArrow from "../../assets/white-arrow.svg"
 
 function Button ({ arrow, buttonStyle, loading, children, ...props }) {
     return (
-        <button className={`button ${buttonStyle}`}>
-            {children}           
+        <button className={`button ${buttonStyle}`} {...props}>
+            {children} {arrow && <img src={WhiteArrow} />}           
         </button>
     )
 }
