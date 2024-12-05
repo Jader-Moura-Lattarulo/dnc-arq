407bf9a09824477ed31b5bca793f5f0aeacd41fd
Jader Moura <jader.dev.moura@gmail.com>
2024-12-05 10:02:04 -0300

    Finalizado controle de formulário e inserido API para envio e recebimento de email

--- src/components/ProjectsList/ProjectsList.jsx
+++ src/components/ProjectsList/ProjectsList.jsx
@@ -32,17 +32,19 @@ function ProjectsList() {
             </div>
             <div className="projects-grid">
                 {
-                    projects.map((project) => (
-                        <div className="project-card d-flex jc-center al-center fd-column" key={project.id}>
-                            <div 
-                                className="thumb tertiary-background" 
-                                style={{ backgroundImage: `url(${project.thumb})` }}        
-                            ></div>        
-                            <h3>{project.title}</h3>
-                            <p>{project.subtitle}</p>
-                            <img src={LikedFilled} height="20px"/>
-                        </div>
-                    ))
+                    projects ?
+                        projects.map((project) => (
+                            <div className="project-card d-flex jc-center al-center fd-column" key={project.id}>
+                                <div 
+                                    className="thumb tertiary-background" 
+                                    style={{ backgroundImage: `url(${project.thumb})` }}        
+                                ></div>        
+                                <h3>{project.title}</h3>
+                                <p>{project.subtitle}</p>
+                                <img src={LikedFilled} height="20px"/>
+                            </div>
+                        )) 
+                    : null
                 }
             </div>
         </div>
